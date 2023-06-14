bc9de98151e4c58d4a582b433bab224ee87e3361
joseRamirezgomez <joseramirezgomez00@gmail.com>
2023-06-14 08:31:33 -0500

    comandos sql

---
+++ ejercicio01/CRUD SQL.sql
@@ -0,0 +1,9 @@
+-- hemos utilizado estas palabras reservadas;;;
+-- CREATE DATABASE (para crear una nueva BD
+ CREATE DATABASE <db>;
+ --SHOW DATABASE (para visualizar las BD creadas)
+ SHOW DATABASE;
+ -- USE (para hacer uso de una BD específica)
+ USE <db>;
+ -- SHOW TABLES (para visualizar las tablas creadas en la BD usada)
+SHOW TABLES;
